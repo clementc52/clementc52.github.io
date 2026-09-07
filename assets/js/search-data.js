305751d9cf8973cb762f0f3a7e4c28e20c28028f
clementc52 <pansbakeryofficials@gmail.com>
2026-09-07 12:30:12 +0000

    Deploying to gh-pages from @ clementc52/clementc52.github.io@7993356662b0cbe2c2096286b8065305bb93fb75 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-teaching",
+          title: "Teaching",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
